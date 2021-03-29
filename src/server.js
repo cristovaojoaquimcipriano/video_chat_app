dc1af9e5b4f4881cb45e41b446687bee46410b95
cristovaojoaquimcipriano <cristovaojoaquimcipriano@gmail.com>
2021-03-29 22:38:55 +0100

    Update config Peer

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.broadcast.emit("user-connected", userId);
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
     });
